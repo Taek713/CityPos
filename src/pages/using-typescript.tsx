0e5b4e12f10c64c1ea2c4a9f1d1a106a7331cc7d
Taek713 <tae.kim147@gmail.com>
2021-12-20 11:49:53 -0500

    New additions fr template

--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -14,26 +14,16 @@ type DataProps = {
 const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
   <Layout>
     <Seo title="Using TypeScript" />
-    <h1>Gatsby supports TypeScript by default!</h1>
     <p>
-      This means that you can create and write <em>.ts/.tsx</em> files for your
-      pages, components etc. Please note that the <em>gatsby-*.js</em> files
-      (like gatsby-node.js) currently don't support TypeScript yet.
-    </p>
-    <p>
-      For type checking you'll want to install <em>typescript</em> via npm and
-      run <em>tsc --init</em> to create a <em>tsconfig</em> file.
-    </p>
-    <p>
-      You're currently on the page "{path}" which was built on{" "}
-      {data.site.buildTime}.
-    </p>
-    <p>
-      To learn more, head over to our{" "}
-      <a href="https://www.gatsbyjs.com/docs/typescript/">
-        documentation about TypeScript
-      </a>
-      .
+     내가 있음으로 자신이 한 번 더 돌아봐지고 내가 있음으로
+      자기를 더 아름답게 가꾸고 자신을 망치는 것들과 치열하게 싸워 나가는
+      아름다운 등불로 걸어가야겠구나. 나이 들수록 더 푸르고 향기 나는 아름다운
+      사람의 등불로 다시 그 등불 아래로 </p><br></br><p>Because of my presence, I have to look
+      back myself once more. My presence would make me more beautiful, and force
+      me to fight against the bad things destroying myself. So I have to stride
+      toward the beautiful lamp. It will be more promising and fragrant while
+      getting older It must be the lamp of a beautiful person. Once again, it
+      invites us to under the lamp.
     </p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
